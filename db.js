0ca1bd34e653c6345fe33306c5888cf286f78160
bimscout <github@bimscout.com>
2016-01-23 17:28:51 -0800

    updated package.json to server.js instead of boot.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var url = process.env.MONGOLAB_URI || 'mongodb://localhost/lego-instructions'
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost/lego-instructions';
 mongoose.connect(url, function() {
     console.log('mongodb connected')
 });
