451dd4c1490a46dd189b7d38afd5d4c8bc0836fa
bimscout <github@bimscout.com>
2016-02-11 20:19:07 -0800

    Updated admin page, missing Update and Add
    I think the page logic is working, but I still need to hook up the
    Update and Add functionality.

--- components/admin/admin.directive.js
+++ components/admin/admin.directive.js
@@ -16,6 +16,94 @@ function admin() {
     return directive;
 }
 
-function AdminController() {
+function AdminController(themeListSvc, productListSvc, instructionListSvc) {
     var vm = this;
+    themeListSvc.fetch()
+        .then(function (themes) {
+            vm.themes = themes.data;
+        }, function (response) {
+            $scope.data = response.data || "Request failed";
+            $scope.status = response.status;
+        });
+
+    //  Themes
+    vm.activeTheme = null;
+    vm.addingTheme = false;
+
+    vm.addTheme = function() {
+        vm.activeTheme = null;
+        vm.activeProduct = null;
+        vm.activeInstruction = null;
+        vm.addingTheme = true;
+    };
+    vm.updateTheme = function() {
+
+    };
+    vm.saveNewTheme = function() {
+
+    };
+    vm.cancelNewTheme = function() {
+        vm.addingTheme = false;
+    };
+
+    //  Products
+    vm.activeProduct = null;
+    vm.addingProduct = false;
+
+    vm.fetchProducts = function(theme) {
+        productListSvc.fetch(theme._id)
+            .then(function (products) {
+                vm.products = products.data;
+            }, function (response) {
+                $scope.data = response.data || "Request failed";
+                $scope.status = response.status;
+            });
+        vm.activeTheme = theme;
+        vm.activeProduct = null;
+        vm.activeInstruction = null;
+    };
+    vm.addProduct = function() {
+        vm.activeInstruction = null;
+        vm.activeProduct = null;
+        vm.addingProduct = true;
+    };
+    vm.saveNewProduct = function() {
+
+    };
+    vm.cancelNewProduct = function() {
+        vm.addingProduct = false;
+    };
+    vm.updateProduct = function() {
+
+    };
+
+    //  Instructions
+    vm.activeInstruction = null;
+    vm.addingInstruction = false;
+
+    vm.fetchInstructions = function(product) {
+        instructionListSvc.fetch(product._id)
+            .then(function (instructions) {
+                vm.instructions = instructions.data;
+            }, function (response) {
+                $scope.data = response.data || "Request failed";
+                $scope.status = response.status;
+            });
+        vm.activeProduct = product;
+        vm.activeInstruction = null;
+    };
+    vm.addInstruction = function() {
+        vm.activeInstruction = null;
+        vm.addingInstruction = true;
+    };
+    vm.saveNewInstruction = function() {
+
+    };
+    vm.cancelNewInstruction = function() {
+        vm.addingInstruction = false;
+    };
+    vm.updateInstruction = function() {
+
+    };
+
 }
